5ef2a0f321b57edbd6994b690e73de3dd18acbdd
gangukss <kwk7328@gmail.com>
2022-01-14 04:00:21 +0900

    fix typo in config.js

--- config/config.js
+++ config/config.js
@@ -16,8 +16,8 @@ module.exports = {
   production: {
     username: process.env.DB_USERNAME,
     password: process.env.DB_PASSWORD,
-    database: process.env.DB_USERNAME,
-    host: process.env.DB,
+    database: process.env.DB,
+    host: process.env.HOST,
     dialect: 'mysql'
   }
 }
